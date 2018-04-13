e2c4fb6020e313926d0c20bfff481b41d44911ae
minhnguyet206 <minhnguyet200698@gmail.com>
2018-04-13 19:37:16 +0700

    them project Client-angular & Navbar

---
+++ client-angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignInComponent,
+    NavbarComponent
+    
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

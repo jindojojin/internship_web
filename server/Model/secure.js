ee22baa992f9cec5470b3b3ad35d39b8044a9409
Tran Quang Linh <tranquanglinh.pt@gmail.com>
2018-04-18 15:31:23 +0700

    them chuc nang tao jwt và xác thực jwt

--- server/Model/secure.js
+++ server/Model/secure.js
@@ -1,5 +1,7 @@
 var crypto = require('crypto');
-module.exports = {
+var jwt = require('jsonwebtoken');
+const jwtsecure = "messagefromserver";
+var secure = {
     createSalt : function(){
         return crypto.randomBytes(10).toString('hex');
     },
@@ -8,6 +10,23 @@ module.exports = {
     },
     compare : function(str,hash,salt){
         return hash == encrypt(str,salt);
+    },
+    createUserToken: function(user){
+        return jwt.sign(user,jwtsecure,{expiresIn : 86400});
+    },
+    verifyUserToken: function(token){
+        return jwt.verify(token,jwtsecure)
     }
 }
+module.exports = secure;
+// var u = {
+//     username: "adsfasdfasdf",
+//     password: "adfafa",
+// }
+// // var a=secure.createUserToken(u);
+// var b=secure.verifyUserToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkc2Zhc2RmYXNkZiIsInBhc3N3b3JkIjoiYWRmYWZhIiwiaWF0IjoxNTI0MDM4NzQ2LCJleHAiOjE1MjQwNDg3NDZ9.iRX1hY_vtSwRKp-SyUJLqe4r307ftdkvViccD6QbMRU");
+// // console.log(a);
+// console.log(b);
+
+
 

c40735440dc4e34660297c0a005dfbe4c9a26304
Tran Quang Linh <tranquanglinh.pt@gmail.com>
2018-04-18 21:07:04 +0700

    gửi usertoken cho client thành công

--- server/Control/signin_router.js
+++ server/Control/signin_router.js
@@ -1,9 +1,11 @@
 const user = require('../Model/userModel');
 module.exports = {
     validate_user: function(req,res){
-        var username = req.body.username;
-        var password = req.body.password;
-        
+        let username = req.body.username;
+        let password = req.body.password;
+        let result = user.checkUser(username,password)
+        .then(userToken => res.send(userToken))
+        .catch( e => {console.log(e); res.send("bạn đã nhập sai tên đăng nhập hoặc mật khẩu")});
     },
     change_password: function(req,res){
         var username = req.body.username;
--- server/Model/userModel.js
+++ server/Model/userModel.js
@@ -2,24 +2,28 @@ var secure = require('./secure')
 var database_query = require('./DatabaseModel/database_query');
 var userModel = {
     checkUser: async function (username, password) {
-        await database_query.getUser(username)
-            .then(res => {
-                let salt = res.salt;
-                let hash = res.password;
-                if(secure.compare(password,hash,salt)){                    
-                    let token = secure.createUserToken(res);
-                    let user={username:res.username,nickname:res.nickname,usertoken:token};
-                    return Promise.resolve(user);
-                }else{
-                    return Promise.reject(new Error("matkhaukhongdung"))
-                }
-            })
-            .catch(e => {
-                return Promise.reject(e);
+        try {
+            var res = await database_query.getUser(username);
+            let salt = res.salt;
+            let hash = res.password;
+            if (secure.compare(password, hash, salt)) {
+                let token = secure.createUserToken(res);
+                var user = { username: res.username, nickname: res.nickname, usertoken: token };
+                console.log(user);
+                return Promise.resolve(user);
+            } else {
+                return Promise.reject(new Error("mat khau khong dung"));
             }
-            )
+        } catch (error) {
+            return Promise.reject(e);
+        }
     }
 }
 module.exports = userModel;
-console.log("ádfsadfasdfasdf");
-userModel.checkUser("16021031","16021031").then(res => console.log(res)).catch( e => console.log("xay ra loi"));
+
+// var a = async function () {
+//     var a = await userModel.checkUser("16021031", "16021031").then(user => console.log(user))
+//         .catch(e => console.log(e));
+//     console.log("ádfsadfasdfasdf");
+// }
+// a();

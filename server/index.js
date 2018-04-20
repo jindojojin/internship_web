2b9d1fc79648f716ced24da8d6e21f4c9f970241
Tran Quang Linh <tranquanglinh.pt@gmail.com>
2018-04-20 10:40:19 +0700

    xoa bo server

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.listen(3000,function(){
-    console.log("server dang chay"); 
-});
-var control = require("./Control/control.js");
-control.route(app);
-

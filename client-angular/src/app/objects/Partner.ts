09033aab173faee677766844212676ee79048f38
Tran Quang Linh <tranquanglinh.pt@gmail.com>
2018-04-20 19:20:45 +0700

    hien thi cac partner duoi dang dardboard

---
+++ client-angular/src/app/objects/Partner.ts
@@ -0,0 +1,4 @@
+export class Partner{
+    name:string;
+    logo:string;
+}
--- client-angular/src/app/partner-card/partner-card.component.ts
+++ client-angular/src/app/partner-card/partner-card.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {Icon} from 'octicons'
+import { Component, OnInit, Input } from '@angular/core';
+import { Partner } from '../objects/Partner';
+
 
 @Component({
   selector: 'app-partner-card',
@@ -7,11 +8,14 @@ import {Icon} from 'octicons'
   styleUrls: ['./partner-card.component.css']
 })
 export class PartnerCardComponent implements OnInit {
-  partnerName:string= "VNG CORP operation";
-  logoPartner:string = "https://itviec.com/system/production/employers/logos/31/kms-technology-logo-170-151.png?1454113898";
-  constructor() { }
+  @Input() partnerName:string ;
+  @Input() logoPartner:string;
+  
+  constructor() {
+  }
 
   ngOnInit() {
+    
   }
 
 }

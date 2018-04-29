52fbf83df5a34a2aaf805b78474191f87d1bdebc
minhnguyet206 <minhnguyet200698@gmail.com>
2018-04-29 23:04:21 +0700

    thm component menu-search-bar

---
+++ client-angular/src/app/components/menu-search-bar/menu-search-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-search-bar',
+  templateUrl: './menu-search-bar.component.html',
+  styleUrls: ['./menu-search-bar.component.css']
+})
+export class MenuSearchBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

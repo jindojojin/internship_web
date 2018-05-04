632e1c1d66a396eee3ea852ff930ff8fcb7be500
unknown <tranquanglinh.pt@gmai.com>
2018-05-04 13:59:10 +0700

    màn hình chức năng của giảng viên

--- client-angular/src/app/lecturer/list-student-follow-me/list-student-follow-me.component.ts
+++ client-angular/src/app/lecturer/list-student-follow-me/list-student-follow-me.component.ts
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-student-follow-me.component.css']
 })
 export class ListStudentFollowMeComponent implements OnInit {
-
+  studentFollowMe: any[] = [
+    {
+      name: "trần quang linh",
+      studentCode: "16021031",
+      vnumail: "16021031@vnu.edu.vn"
+    },
+    {
+      name: "trần quang lanh",
+      studentCode: "16021021",
+      vnumail: "16021021@vnu.edu.vn"
+    },
+    {
+      name: "trần quang lĩnh",
+      studentCode: "16021033",
+      vnumail: "16021033@vnu.edu.vn"
+    },
+    {
+      name: "trần quang linh",
+      studentCode: "16021031",
+      vnumail: "16021031"
+    },]
   constructor() { }
 
   ngOnInit() {

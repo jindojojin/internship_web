71a5c4e4b0fad3fe22bba4a49500f62fbe82715c
minhnguyet206 <minhnguyet200698@gmail.com>
2018-05-17 23:31:39 +0700

    giao dien chi tiet cong viec theo form

---
+++ client-angular/src/app/partner/form-partner/form-partner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-partner',
+  templateUrl: './form-partner.component.html',
+  styleUrls: ['./form-partner.component.css']
+})
+export class FormPartnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

2dafc7c991d9ae69479b7197b65c9201ec76e29e
linhtq <tranquanglinh.pt@gmail.com>
2018-06-01 20:11:17 +0700

    thêm tính năng theo dõi, bỏ theo dõi partner

--- client-angular/src/app/components/partner-list/partner-list.component.ts
+++ client-angular/src/app/components/partner-list/partner-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PartnerListService } from './partner-list.service';
 import { myWebsiteDomain } from '../../objects/appConfig';
+import { StudentService } from '../../student.service';
 
 @Component({
   selector: 'app-partner-list',
@@ -11,9 +12,32 @@ import { myWebsiteDomain } from '../../objects/appConfig';
 
 export class PartnerListComponent implements OnInit {
   partners: Object;
- server = myWebsiteDomain;
-  constructor(private partnerListService: PartnerListService) { }
-
+  server = myWebsiteDomain;
+  constructor(private partnerListService: PartnerListService, private studentService: StudentService) { }
+  follow(partnerID,partnerName) {
+    this.studentService.crudFollow("follow", "partner", partnerID)
+      .then(
+        r => {
+          let message = "Đã thêm "+partnerName +" vào danh sách yêu thích, bạn sẽ nhận được các thông báo mới nhất từ "+partnerName;
+          window.alert(message);
+          this.ngOnInit();
+        })
+      .catch(e => {
+        window.alert("đã xảy ra lỗi, vui lòng thử lại sau")
+      })
+  }
+  unfollow(partnerID,partnerName) {
+    this.studentService.crudFollow("unfollow", "partner", partnerID)
+    .then(
+      r => {
+        let message = "Đã bỏ theo dõi "+partnerName;
+        window.alert(message);
+        this.ngOnInit();
+      })
+    .catch(e => {
+      window.alert("đã xảy ra lỗi, vui lòng thử lại sau")
+    })
+  }
   ngOnInit() {
     this.partnerListService.getList()
       .then(res => {

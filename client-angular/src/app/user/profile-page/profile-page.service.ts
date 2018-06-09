286eb01585203a1c983fba908865e9c04d63d13a
linhtq <tranquanglinh.pt@gmail.com>
2018-06-09 20:10:11 +0700

    update database

--- client-angular/src/app/user/profile-page/profile-page.service.ts
+++ client-angular/src/app/user/profile-page/profile-page.service.ts
@@ -1,17 +1,19 @@
-import { Injectable } from "@angular/core";
-import { Http, Headers } from "@angular/http";
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
-import { getCookie } from "../../objects/Cookiee";
-import { myWebsiteDomain } from "../../objects/appConfig";
+import { getCookie } from '../../objects/Cookiee';
+import { myWebsiteDomain } from '../../objects/appConfig';
 
 @Injectable()
 
 export class ProfilePageService {
     constructor(private http: Http) { }
-    getProfile(id) { 
+    getProfile(id) {
+        // tslint:disable-next-line:prefer-const
         let userID = id;
-        var url = myWebsiteDomain+"/profile/id="+ userID; //api server trả về
-        return this.http.get(url,{ withCredentials: true })
+        // tslint:disable-next-line:prefer-const
+        let url = myWebsiteDomain + '/profile/id=' + userID; // api server trả về
+        return this.http.get(url, { withCredentials: true })
             .toPromise()
             .then(res => {
                 // console.log(res.json());
@@ -20,13 +22,28 @@ export class ProfilePageService {
             .catch(err => console.log(err));
     }
 
-    updateProfile(value){
-        let url = myWebsiteDomain+"/user/update/profile";
-        return this.http.post(url , value , { withCredentials: true})
-        .toPromise()
-        .then(res => {
-            
-        })
-        .catch(err => console.log(err));
+    updateProfile(value) {
+        // tslint:disable-next-line:prefer-const
+        let url = myWebsiteDomain + '/user/update/profile';
+        return this.http.put(url, value, { withCredentials: true })
+            .toPromise()
+            .then(res => {
+
+            })
+            .catch(err => console.log(err));
+    }
+
+    adminUpdateProfile(userID, value) {
+        // tslint:disable-next-line:prefer-const
+        let url = myWebsiteDomain + '/admin/update/profile/userID=' + userID;
+        return this.http.put(url, value, { withCredentials: true })
+            .toPromise()
+            .then(res => {
+                // tslint:disable-next-line:triple-equals
+                if ( res.status == 201) {
+                    return true;
+                } else { return false; }
+            })
+            .catch(err => console.log(err));
     }
 }

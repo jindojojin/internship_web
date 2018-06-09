272fb11f12ff4dddc157ebd3414cc1ab40c141a9
linhtq <tranquanglinh.pt@gmail.com>
2018-06-09 17:17:43 +0700

    partner phải đổi mật khẩu trong lần đăng nhập đầu tiên

--- client-angular/src/app/navbar/sign-in/sign-in.component.ts
+++ client-angular/src/app/navbar/sign-in/sign-in.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
 import { SignInService } from './sign-in.service';
 import { setCookie } from '../../objects/Cookiee';
+import { ChangePasswordModalComponent } from '../../user/change-password-modal/change-password-modal.component';
+
 
 @Component({
   selector: 'app-sign-in',
@@ -11,27 +13,39 @@ import { setCookie } from '../../objects/Cookiee';
 
 export class SignInComponent implements OnInit {
   @Output() loggedIn = new EventEmitter();
+  @ViewChild(ChangePasswordModalComponent)
+  changePassword: ChangePasswordModalComponent;
+
 
   constructor(private signInService: SignInService) { }
 
   ngOnInit() {
   }
 
-  onSubmit(formSignIn) {
+  async onSubmit(formSignIn) {
     this.signInService.sendPost(formSignIn.value)
       .then(result => {
-        window.location.reload();
         console.log(result);
-        setCookie("nickname", result.nickname, 2);
-        setCookie("userID", result.userID, 2);
-        setCookie("userType", result.usertype, 2);
-        setCookie("userToken", result.usertoken, 2);
-        console.log("đã lưu cookies");
-        document.getElementById("closeModal").click();
-        this.loggedIn.emit();
+        if (result.usertype === 'partner' && result.passwordChanged === 0) {
+          console.log('xác định là phải đổi mật khẩu');
+          setCookie('userID', result.userID, 2);
+          setCookie('userToken', result.usertoken, 2);
+          this.changePassword.showModal();
+          document.getElementById('closeModal').click();
+        } else {
+          window.location.reload();
+          setCookie('nickname', result.nickname, 2);
+          setCookie('userID', result.userID, 2);
+          setCookie('userType', result.usertype, 2);
+          setCookie('userToken', result.usertoken, 2);
+          console.log('đã lưu cookies');
+          document.getElementById('closeModal').click();
+          this.loggedIn.emit();
+        }
       })
       .catch(err => {
-        document.getElementById("error").innerHTML = "Tên đăng nhập hoặc mật khẩu không đúng!"
+        console.log(err);
+        document.getElementById('error').innerHTML = 'Tên đăng nhập hoặc mật khẩu không đúng!';
       });
   }
 }
--- client-angular/src/app/navbar/sign-in/sign-in.service.ts
+++ client-angular/src/app/navbar/sign-in/sign-in.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
-import { myWebsiteDomain } from "../../objects/appConfig";
+import { myWebsiteDomain } from '../../objects/appConfig';
 
 @Injectable()
 
@@ -9,10 +9,10 @@ export class SignInService {
     constructor(private http: Http) { }
 
     sendPost(value) {
-        const url = myWebsiteDomain + "/signin";
-        const headers = new Headers({'Content-Type': 'application/json'})
+        const url = myWebsiteDomain + '/signin';
+        const headers = new Headers({'Content-Type': 'application/json'});
         const body = JSON.stringify(value);
-        return this.http.post(url, body, { headers },)
+        return this.http.post(url, body, { headers })
             .toPromise()
             .then(res => res.json())
             .catch(err => console.log(err));
